53fa7b1fc3c2bf202dbca1a22a01412a8cf94cff
Stefano Lista <listafefo@gmail.com>
2022-06-13 18:32:49 -0300

    se implemento la correccion de la entrega previa y se agregaron los botones con contador

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,13 +1,42 @@
-import React from "react";
+import { render } from "@testing-library/react";
+import React, { useState  }from "react";
 import { ReactDOM } from "react";
 
-export default function ItemListContainer () {
+
+
+const BotonCounter = () => {
+    const [num, setNum] = useState(0);
+    const incNum = () => {
+           setNum(num + 1);  
+    };
+
+    const decNum = () => {
+        if(num > 0) {
+        setNum(num - 1);
+        }else{
+            setNum(0);
+        }
+    };
+
+    return (
+        <div className="CounterContainer">
+            <button className="BotonCounter" onClick={decNum}>-</button>
+            <h3 className="BotonCounter">{num}</h3>
+            <button className="BotonCounter" onClick={incNum}>+</button>
+        </div>
+    )
+}
+
+
+export default function ItemListContainer (props) {
     return (
         <div className="itemCatalogo">
-            Placeholder para futuros items de la tienda
+        <p nombreItem=''>{props.nombreItem}</p>
+        <p contenido=''>{props.contenido}</p>
+        <p precioItem=''>{props.precioItem}</p>
+        <p className="stockData">Stock: </p>
+        <p stockValue='' className="stockData" >{props.stockValue}</p>
+        <BotonCounter/>
         </div>
     );
 }
-   
-    
-        
